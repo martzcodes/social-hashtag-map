defa48c44942ab94a0241cbd021c79e51e3447d0
OEHokie <matt.martz@gmail.com>
2015-10-01 00:43:04 -0400

    typo

--- run_celery.sh
+++ run_celery.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
- 
+
+export DJANGO_PROJECT_DIR=/app/social-hashtag-map
 cd social-hashtag-map
 # run Celery worker for our project social-hashtag-map with Celery configuration stored in Celeryconf
-su -m myuser -c "celery -A polls worker -B"
+su -m myuser -c "celery -A config worker -B"
