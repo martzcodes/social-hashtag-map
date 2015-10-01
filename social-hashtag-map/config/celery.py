e65100489a36c210cff3f7e2d703b0d880a544a6
OEHokie <matt.martz@gmail.com>
2015-10-01 01:10:31 -0400

    tweaks

--- social-hashtag-map/config/celery.py
+++ social-hashtag-map/config/celery.py
@@ -9,11 +9,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 from django.conf import settings
 
-REDIS_PORT = 6379
-REDIS_DB = 0
-REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')
-app = Celery('config', broker='redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB))
-
+#REDIS_PORT = 6379
+#REDIS_DB = 0
+#REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')
+#app = Celery('config', broker='redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB))
+app = Celery('config', broker='redis://redis')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
